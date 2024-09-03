88c2a78002cea5da6e737e576d834a3cd95e42ab
MarvinWintersteller <Marvin.Wintersteller@campus.lmu.de>
2024-09-03 17:45:27 +0200

    Description of variables for biopsy_sample fixed

--- R/data.R
+++ R/data.R
@@ -5,15 +5,15 @@
 #'
 #' @format A data frame with `r nrow(biopsy_sample)` rows and `r ncol(biopsy_sample)` variables:
 #' \describe{
-#'   \item{V2}{Numeric variable representing [Description of V2].}
-#'   \item{V3}{Numeric variable representing [Description of V3].}
-#'   \item{V4}{Numeric variable representing [Description of V4].}
-#'   \item{V5}{Numeric variable representing [Description of V5].}
-#'   \item{V6}{Numeric variable representing [Description of V6].}
-#'   \item{V7}{Numeric variable representing [Description of V7].}
-#'   \item{V8}{Numeric variable representing [Description of V8].}
-#'   \item{V9}{Numeric variable representing [Description of V9].}
-#'   \item{V10}{Numeric variable representing [Description of V10].}
+#'   \item{V2}{(clump thickness): Thickness of the cell clumps. Scored on a scale from 1 to 10, with higher values indicating thicker cell clumps.}
+#'   \item{V3}{(uniformity of cell size): Uniformity of cell size. Scored on a scale from 1 to 10, with higher values indicating greater differences in cell size.}
+#'   \item{V4}{(uniformity of cell shape): Uniformity of cell shape. Scored on a scale from 1 to 10, with higher values indicating greater differences in cell shape.
+#'   \item{V5}{(marginal adhesion): Marginal adhesion, or the ability of cells to stick together at the edges. Scored on a scale from 1 to 10, with higher values indicating stronger adhesion.}
+#'   \item{V6}{(single epithelial cell size): Size of the individual epithelial cells. Scored on a scale from 1 to 10, with higher values indicating larger epithelial cells.}
+#'   \item{V7}{(bare nuclei): Number of bare nuclei, which are nuclei not surrounded by cytoplasm. Scored on a scale from 1 to 10, with 16 values missing in the dataset.}
+#'   \item{V8}{bland chromatin): Appearance of the chromatin found within the cell nuclei. Scored on a scale from 1 to 10, with higher values indicating less densely packed chromatin.}
+#'   \item{V9}{(normal nucleoli): Number of normal nucleoli in the cell nuclei. Scored on a scale from 1 to 10, with higher values indicating a greater number of nucleoli.}
+#'   \item{V10}{(mitoses): Number of cell divisions (mitoses). Scored on a scale from 1 to 10, with higher values indicating a higher rate of cell division.}
 #' }
 #'
 #' @source
